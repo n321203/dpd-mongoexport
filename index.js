e95e5dee5307bf52ec1fa76e033db7e17e2ae115
n321203 <n321203@gmail.com>
2015-08-10 15:58:39 +0200

    2

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var util = require('util');
 var internalClient = require('deployd/lib/internal-client')
 var spawn = require('child_process').spawn;
 
-var DPD_MONGOEXPORT_KEY = process.env.DPD_MONGOEXPORT_KEY
+var DPD_MONGOEXPORT_KEY = process.env.DPD_MONGOEXPORT_KEY;
+var DB_USERNAME 		= process.env.DB_USERNAME;
+var DB_PASS 			= process.env.DB_PASS;
+var DB_NAME 			= process.env.DB_NAME;
 
 function MongoExport(name, options) {
 	Resource.apply(this, arguments);
@@ -15,70 +18,68 @@ module.exports = MongoExport;
 MongoExport.prototype.clientGeneration = true;
 MongoExport.prototype.handle = function (ctx, next) {
 
-	console.log("disabled")
+	var dpd = internalClient.build(process.server);
 
-	// var dpd = internalClient.build(process.server);
+    // Validate
+    var body = ctx.req.body || {}
+    if(!body || !body.k || body.k != DPD_MONGOEXPORT_KEY){
+    	return next();
+    }
 
- //    // Validate
- //    var body = ctx.req.body || {}
- //    if(!body || !body.k || body.k != DPD_MONGOEXPORT_KEY){
- //    	return next();
- //    }
+    // Export fields:
+    var fields = "username,loans.0.firstName,loans.0.lastName,_id,token,loans.0.loan,loans.0.address,loans.0.zip,loans.0.city,loans.0.municipality,loans.0.phone,loans.0.income,loans.0.bank,created,loans.0.created"
+    fields += ",hasAcceptedOffer,hasDeclinedOffer"
+    fields += ",hasAcceptedOffer201411,hasDeclinedOffer201411"
+    fields += ",hasAcceptedOfferSthlm201412,hasDeclinedOfferSthlm201412"
+    fields += ",hasAcceptedOfferRekarne,hasDeclinedOfferRekarne"
+    fields += ",hasAcceptedOfferSEBJagersro,hasDeclinedOfferSEBJagersro"
+    fields += ",hasAcceptedOfferSHBMalmo,hasDeclinedOfferSHBMalmo"
+    fields += ",hasAcceptedOfferLFSkane,hasDeclinedOfferLFSkane"
+    fields += ",loans.0.name,declineReason,declineReason201411,declineReasonSthlm201412"
+    fields += ",hasShared,referralId,referredById,notes,isAdmin,name,socialAccount"
 
- //    // Export fields:
- //    var fields = "username,loans.0.firstName,loans.0.lastName,_id,token,loans.0.loan,loans.0.address,loans.0.zip,loans.0.city,loans.0.municipality,loans.0.phone,loans.0.income,loans.0.bank,created,loans.0.created"
- //    fields += ",hasAcceptedOffer,hasDeclinedOffer"
- //    fields += ",hasAcceptedOffer201411,hasDeclinedOffer201411"
- //    fields += ",hasAcceptedOfferSthlm201412,hasDeclinedOfferSthlm201412"
- //    fields += ",hasAcceptedOfferRekarne,hasDeclinedOfferRekarne"
- //    fields += ",hasAcceptedOfferSEBJagersro,hasDeclinedOfferSEBJagersro"
- //    fields += ",hasAcceptedOfferSHBMalmo,hasDeclinedOfferSHBMalmo"
- //    fields += ",hasAcceptedOfferLFSkane,hasDeclinedOfferLFSkane"
- //    fields += ",loans.0.name,declineReason,declineReason201411,declineReasonSthlm201412"
- //    fields += ",hasShared,referralId,referredById,notes,isAdmin,name,socialAccount"
+	// From & to
+    var createdFrom		= (body.createdFrom) ? body.createdFrom : "2010-01-01"
+    var createdTo  		= (body.createdTo) ? body.createdTo : "2020-01-01";
+    var loanFrom        = (body.loanFrom) ? body.loanFrom : "2010-01-02";
+    var loanTo          = (body.loanTo) ? body.loanTo : "2020-01-01";
 
-	// // From & to
- //    var createdFrom		= (body.createdFrom) ? body.createdFrom : "2010-01-01"
- //    var createdTo  		= (body.createdTo) ? body.createdTo : "2020-01-01";
- //    var loanFrom        = (body.loanFrom) ? body.loanFrom : "2010-01-02";
- //    var loanTo          = (body.loanTo) ? body.loanTo : "2020-01-01";
+    if(loanFrom && loanFrom != "2010-01-02"){
+        var dbquery     = '{ loans: { $elemMatch: { created: {$gte: "' + loanFrom + '"}, created: {$lte:"' + loanTo + '"} } } }' 
+    }
+    else 
+        var dbquery     = "{ created: {$gte: \"" + createdFrom + "\"}, created:{$lte: \"" + createdTo + "\"} }"
 
- //    if(loanFrom && loanFrom != "2010-01-02"){
- //        var dbquery     = '{ loans: { $elemMatch: { created: {$gte: "' + loanFrom + '"}, created: {$lte:"' + loanTo + '"} } } }' 
- //    }
- //    else 
- //        var dbquery     = "{ created: {$gte: \"" + createdFrom + "\"}, created:{$lte: \"" + createdTo + "\"} }"
+   var params = [ 
+		'--host', 'paulo.mongohq.com',
+		'--port', 10006,
+		'--db', DB_NAME,
+		'--username', DB_USERNAME,
+		'--password', DB_PASS]
+	params.push('--collection', 'users', "--fields", fields, "--query", dbquery, "--csv")
+	//params.push('--query', '{ loans: { $elemMatch: { created: {$gte: "2014-12-01"}, created: {$lte:"2015-01-01"} } } }')
+	var mongoExport = spawn('mongoexport', params)
 
- //   var params = [ 
-	// 	'--host', 'paulo.mongohq.com',
-	// 	'--port', 10006,
-	// 	'--db', 'app18800740',
-	// 	'--username', 'heroku',
-	// 	'--password', 'dbpassword@123!']
-	// params.push('--collection', 'users', "--fields", fields, "--query", dbquery, "--csv")
-	// //params.push('--query', '{ loans: { $elemMatch: { created: {$gte: "2014-12-01"}, created: {$lte:"2015-01-01"} } } }')
-	// var mongoExport = spawn('mongoexport', params)
-
-	// // Results:
-	// var result = "";
-	// mongoExport.stdout.on('data', function(data) {
-	// 	result += data;
-	// });
- //    mongoExport.on('close', function(code) {
+	// Results:
+	var result = "";
+	mongoExport.stdout.on('data', function(data) {
+		result += data;
+	});
+    mongoExport.on('close', function(code) {
     	
- //    	// Replace all commas within quotes (in comments etc) with something else
- //    	result = result.replace(/".*?"/g, function(str) {
- //    		return str.replace(/,/g, '±');
-	// 	});
-	// 	// Now change all commas to tabs
-	// 	result = result.replace(/,/g, "	")
+    	// Replace all commas within quotes (in comments etc) with something else
+    	result = result.replace(/".*?"/g, function(str) {
+    		return str.replace(/,/g, '±');
+		});
+		// Now change all commas to tabs
+		result = result.replace(/,/g, "	")
 
-	// 	// Replace back commas within quotes
- //    	result = result.replace(/".*?"/g, function (str) {
- //    		return str.replace(/±/g, ',');
-	// 	})
+		// Replace back commas within quotes
+    	result = result.replace(/".*?"/g, function (str) {
+    		return str.replace(/±/g, ',');
+		})
 
- //    	result = "<meta charset='utf-8'><pre>" + result + "</pre>"
- //        ctx.done(null, result)
- //    })
+    	result = "<meta charset='utf-8'><pre>" + result + "</pre>"
+        ctx.done(null, result)
+    })
 }
