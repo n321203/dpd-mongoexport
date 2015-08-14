3ac6e699de8bb7e4b594c37360a3d5de1bb66608
n321203 <n321203@gmail.com>
2015-08-14 15:16:46 +0200

    2

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ var internalClient = require('deployd/lib/internal-client')
 var spawn = require('child_process').spawn;
 
 var DPD_MONGOEXPORT_KEY = process.env.DPD_MONGOEXPORT_KEY;
-var DB_USERNAME 		= process.env.DB_USERNAME;
-var DB_PASS 			= process.env.DB_PASS;
-var DB_NAME 			= process.env.DB_NAME;
+
+var mongodbUri = require('mongodb-uri');
+var MONGOHQ_URL = process.env.MONGOHQ_URL || false;
 
 function MongoExport(name, options) {
-	Resource.apply(this, arguments);
+    Resource.apply(this, arguments);
 }
 
 util.inherits(MongoExport, Resource);
@@ -18,12 +18,23 @@ module.exports = MongoExport;
 MongoExport.prototype.clientGeneration = true;
 MongoExport.prototype.handle = function (ctx, next) {
 
-	var dpd = internalClient.build(process.server);
+    if(!MONGOHQ_URL){
+        console.log("Missing environment variable MONGOHQ_URL!");
+        ctx.done({error:"Missing environment variable MONGOHQ_URL"}, null)
+        return;
+    }
+    var uriObject           = mongodbUri.parse(MONGOHQ_URL);
+    var DB_USERNAME         = uriObject.username;
+    var DB_PASS             = uriObject.password;
+    var DB_NAME             = uriObject.database;
+
+
+    var dpd = internalClient.build(process.server);
 
     // Validate
     var body = ctx.req.body || {}
     if(!body || !body.k || body.k != DPD_MONGOEXPORT_KEY){
-    	return next();
+        return next();
     }
 
     // Export fields:
@@ -38,9 +49,9 @@ MongoExport.prototype.handle = function (ctx, next) {
     fields += ",loans.0.name,declineReason,declineReason201411,declineReasonSthlm201412"
     fields += ",hasShared,referralId,referredById,notes,isAdmin,name,socialAccount"
 
-	// From & to
-    var createdFrom		= (body.createdFrom) ? body.createdFrom : "2010-01-01"
-    var createdTo  		= (body.createdTo) ? body.createdTo : "2020-01-01";
+    // From & to
+    var createdFrom     = (body.createdFrom) ? body.createdFrom : "2010-01-01"
+    var createdTo       = (body.createdTo) ? body.createdTo : "2020-01-01";
     var loanFrom        = (body.loanFrom) ? body.loanFrom : "2010-01-02";
     var loanTo          = (body.loanTo) ? body.loanTo : "2020-01-01";
 
@@ -51,35 +62,35 @@ MongoExport.prototype.handle = function (ctx, next) {
         var dbquery     = "{ created: {$gte: \"" + createdFrom + "\"}, created:{$lte: \"" + createdTo + "\"} }"
 
    var params = [ 
-		'--host', 'paulo.mongohq.com',
-		'--port', 10006,
-		'--db', DB_NAME,
-		'--username', DB_USERNAME,
-		'--password', DB_PASS]
-	params.push('--collection', 'users', "--fields", fields, "--query", dbquery, "--csv")
-	//params.push('--query', '{ loans: { $elemMatch: { created: {$gte: "2014-12-01"}, created: {$lte:"2015-01-01"} } } }')
-	var mongoExport = spawn('mongoexport', params)
+        '--host', 'paulo.mongohq.com',
+        '--port', 10006,
+        '--db', DB_NAME,
+        '--username', DB_USERNAME,
+        '--password', DB_PASS]
+    params.push('--collection', 'users', "--fields", fields, "--query", dbquery, "--csv")
+    //params.push('--query', '{ loans: { $elemMatch: { created: {$gte: "2014-12-01"}, created: {$lte:"2015-01-01"} } } }')
+    var mongoExport = spawn('mongoexport', params)
 
-	// Results:
-	var result = "";
-	mongoExport.stdout.on('data', function(data) {
-		result += data;
-	});
+    // Results:
+    var result = "";
+    mongoExport.stdout.on('data', function(data) {
+        result += data;
+    });
     mongoExport.on('close', function(code) {
-    	
-    	// Replace all commas within quotes (in comments etc) with something else
-    	result = result.replace(/".*?"/g, function(str) {
-    		return str.replace(/,/g, '±');
-		});
-		// Now change all commas to tabs
-		result = result.replace(/,/g, "	")
+        
+        // Replace all commas within quotes (in comments etc) with something else
+        result = result.replace(/".*?"/g, function(str) {
+            return str.replace(/,/g, '±');
+        });
+        // Now change all commas to tabs
+        result = result.replace(/,/g, " ")
 
-		// Replace back commas within quotes
-    	result = result.replace(/".*?"/g, function (str) {
-    		return str.replace(/±/g, ',');
-		})
+        // Replace back commas within quotes
+        result = result.replace(/".*?"/g, function (str) {
+            return str.replace(/±/g, ',');
+        })
 
-    	result = "<meta charset='utf-8'><pre>" + result + "</pre>"
+        result = "<meta charset='utf-8'><pre>" + result + "</pre>"
         ctx.done(null, result)
     })
 }
